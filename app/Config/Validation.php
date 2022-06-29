8158c0f593d40c945caa3de2fc319586988eb8aa
zirnich <fauziabusiness@gmail.com>
2022-06-29 18:25:45 +0700

    e-commerce codeigniter

---
+++ app/Config/Validation.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Validation\CreditCardRules;
+use CodeIgniter\Validation\FileRules;
+use CodeIgniter\Validation\FormatRules;
+use CodeIgniter\Validation\Rules;
+
+class Validation
+{
+    //--------------------------------------------------------------------
+    // Setup
+    //--------------------------------------------------------------------
+
+    /**
+     * Stores the classes that contain the
+     * rules that are available.
+     *
+     * @var string[]
+     */
+    public $ruleSets = [
+        Rules::class,
+        FormatRules::class,
+        FileRules::class,
+        CreditCardRules::class,
+        \Myth\Auth\Authentication\Passwords\ValidationRules::class
+    ];
+
+    /**
+     * Specifies the views that are used to display the
+     * errors.
+     *
+     * @var array<string, string>
+     */
+    public $templates = [
+        'list'   => 'CodeIgniter\Validation\Views\list',
+        'single' => 'CodeIgniter\Validation\Views\single',
+    ];
+
+    //--------------------------------------------------------------------
+    // Rules
+    //--------------------------------------------------------------------
+
+    public $register = [
+        'username' => 'alpha_numeric',
+        'password' => 'min_length[1]|alpha_numeric_punct',
+        'confirm' => 'matches[password]'
+    ];
+
+    public $register_errors = [
+        'username' => [
+            'alpha_numeric' => 'Username hanya boleh mengandung huruf dan angka',
+            'is_unique' => 'Username sudah dipakai'
+        ],
+        'password' => [
+            'min_length' => 'Password harus terdiri dari 1 huruf',
+            'alpha_numeric_punct' => 'Password hanya boleh mengandung angka, huruf, dan karakter yang valid'
+        ],
+        'confirm' => [
+            'matches' => 'Konfirmasi password tidak cocok'
+        ]
+    ];
+}
